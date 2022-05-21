ce3b15812e30e36b5c3c8ef0b37709da1d586a2b
Frederick Hoyles <frederick.hoyles@gmail.com>
2022-05-21 13:40:16 +0200

    schema

--- schema.sql
+++ schema.sql
@@ -1,25 +1,29 @@
 DROP SCHEMA IF EXISTS api CASCADE;
 CREATE SCHEMA api;
 
-CREATE TABLE api.events(
-	id integer primary key generated always as identity,
-	created_at TIMESTAMP WITH TIME ZONE default current_timestamp,
-	message TEXT
+CREATE TABLE api.events (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    message TEXT
 );
 
+CREATE INDEX events_created_at ON api.events USING BRIN (created_at);
+
+-- web_anon
 DROP ROLE IF EXISTS web_anon;
-create role web_anon nologin;
+CREATE ROLE web_anon nologin;
+GRANT usage ON SCHEMA api TO web_anon;
+GRANT SELECT ON api.events TO web_anon;
 
-grant usage on schema api to web_anon;
-grant select on api.events to web_anon;
-
+-- authenticator
 DROP ROLE IF EXISTS authenticator;
-create role authenticator noinherit login password 'pw';
-grant web_anon to authenticator;
+CREATE ROLE authenticator noinherit LOGIN PASSWORD 'pw';
+GRANT web_anon TO authenticator;
 
+-- api_user
 DROP ROLE IF EXISTS api_user;
-create role api_user nologin;
-grant api_user to authenticator;
+CREATE ROLE api_user nologin;
+GRANT api_user TO authenticator;
+GRANT usage ON SCHEMA api TO api_user;
+GRANT ALL ON api.events TO api_user;
 
-grant usage on schema api to api_user;
-grant all on api.events to api_user;
