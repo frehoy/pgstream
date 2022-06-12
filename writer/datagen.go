798be3b754e631bdd026baaa1de8566fd3b9863e
Frederick Hoyles <frederick.hoyles@gmail.com>
2022-06-12 20:18:25 +0200

    Fake some search categories

--- writer/datagen.go
+++ writer/datagen.go
@@ -12,6 +12,18 @@ const charset = "abcdefghijklmnopqrstuvwxyz" +
 var seededRand *rand.Rand = rand.New(
 	rand.NewSource(time.Now().UnixNano()))
 
+func randomCategory() string {
+	categories := []string{
+		"Page",
+		"Image",
+		"Map",
+		"Video",
+		"News",
+	}
+	category := categories[rand.Intn(len(categories))]
+	return category
+}
+
 func stringWithCharset(length int, charset string) string {
 	b := make([]byte, length)
 	for i := range b {
@@ -20,25 +32,32 @@ func stringWithCharset(length int, charset string) string {
 	return string(b)
 }
 
-func RandomString(length int) string {
+func randomString(length int) string {
 	return stringWithCharset(length, charset)
 }
 
 type message struct {
 	Message_type string `json:"message_type"`
-	Search_terms string `json:"search_terms"`
+	Search       search `json:"search"`
 }
 
 type payload_struct struct {
 	Message message `json:"message"`
 }
 
-func make_payload() ([]byte, error) {
+type search struct {
+	Query    string `json:"query"`
+	Category string `json:"category"`
+}
 
+func make_payload() ([]byte, error) {
 	data := payload_struct{
 		Message: message{
 			Message_type: "search",
-			Search_terms: RandomString(64),
+			Search: search{
+				Query:    randomString(3),
+				Category: randomCategory(),
+			},
 		},
 	}
 	json_payload, err := json.Marshal(data)
